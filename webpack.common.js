602adc3321a1fd0ed033abbdc6c72370ccebc005
mattfisher <nuggsrocks@yahoo.com>
2020-11-01 01:24:30 -0400

    added production build

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './js/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+			rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			},
+			{
+				test: /\.(jpe?g|png|gif|wav)/,
+				loader: 'file-loader'
+			}
+			]
+		},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			filename: 'index.html',
+			meta: {
+				charset: 'utf-8',
+				viewport: 'width=device-width,initial-scale=1.0'
+			}
+		})
+	]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
+
+module.exports = merge(commonConfig, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				loader: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.scss$/,
+				loader: ['style-loader', 'css-loader', 'sass-loader']
+			},
+		]
+	}
+});
